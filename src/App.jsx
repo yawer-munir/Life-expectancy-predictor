5c317242069fa5c6ce19306f5e2d9d97cb0b5ac6
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2024-08-24 06:46:01 +0000

    buttons done
    setting up two different buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import Button from "./components/Buttons/Button";
 
 function App() {
+  const handleButtonOneClick = () => {
+    alert("Button One clicked!");
+  };
+
+  const handleButtonTwoClick = () => {
+    alert("Button Two clicked!");
+  };
+
   return (
     <div>
-      <h1 className='text-3xl font-bold underline text-teal-800'>Hello world!</h1>
+      <div>
+        <Button
+          onClick={handleButtonOneClick}
+          text='Click Me'
+          className=' shadow-lg'
+        />
+      </div>
+      <div>
+        <Button
+          onClick={handleButtonTwoClick}
+          text='Clic Me Two'
+          className=' shadow-lg bg-red-800'
+        />
+      </div>
     </div>
   );
 }
